8b74152d03caa5934e13c9b2d895584228468575
Aryan Madan <madan.aryy@gmail.com>
2025-12-15 02:30:55 +0530

    removed vconsole

--- index.tsx
+++ index.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import VConsole from 'vconsole';
-
-// Initialize vConsole for on-device debugging
-// This puts a floating green 'vConsole' button on your screen
-new VConsole({ theme: 'dark' });
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
